f72b6d643ab47e81ed8a3b998eb6ec4f09fa11f4
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2016-10-28 13:53:45 +0200

    Trenger ikke lenger style_navbar

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,7 +1,13 @@
 let dropit = document.getElementById("dropit");
 let showthis = document.getElementById("showthis")
 dropit.addEventListener("mouseover", hot);
+dropit.addEventListener("mouseout", cold);
 
 function hot(){
     showthis.classList.toggle("hide");
+    dropit.style.backgroundColor = "blue";
+};
+
+function cold(){
+    dropit.style.backgroundColor = "gold";
 };
