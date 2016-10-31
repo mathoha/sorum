8340f97ed1ef2d35e38b2176fe6189a6b916a70c
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2016-10-31 11:02:27 +0100

    Fortsetter med testing av dropdown js

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,13 +1,32 @@
 let dropit = document.getElementById("dropit");
 let showthis = document.getElementById("showthis")
-dropit.addEventListener("mouseover", hot);
+dropit.addEventListener("mouseover", function() {
+    hot();
+    drops();
+});
 dropit.addEventListener("mouseout", cold);
+showthis.addEventListener("mouseover", hot);
+showthis.addEventListener("mouseout", cold);
 
 function hot(){
-    showthis.classList.toggle("hide");
     dropit.style.backgroundColor = "blue";
+    showthis.style.display = "block";
 };
 
 function cold(){
+    showthis.style.display = "none"
     dropit.style.backgroundColor = "gold";
 };
+
+function drops(){
+    let id = setInterval(frame, 1);
+    let high = 0;
+    function frame(){
+        if (high == 100) {
+            clearInterval(id);
+        } else {
+            high += 5;
+            showthis.style.height = high+'px';
+        }
+    }
+}
