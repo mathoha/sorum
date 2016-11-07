f75ce8243439bfc11b34c78c96ff5aaadd4aff8b
ReidarsOfTheLostArk <reidar.stromme@hotmail.com>
2016-11-07 09:54:51 +0100

    Update slideshow.js

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -2,7 +2,7 @@ var pic=document.getElementById("bildefremvisning")
 bildeArray=["butikkForside.JPG","ekstra1.JPG","ekstra2.JPG","ekstra3.JPG","ekstra4.JPG","ekstra5.JPG"]
 i=0;
 setInterval(function(){
-	pic.src="img/slideshow/"+bildeArray[i];
+	pic.src="scripts/slideshow/"+bildeArray[i];
 	i++;
 	if(i==6){
 		i=0;
