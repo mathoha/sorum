5cda137f0341af4e86df2ad99ad325454093dd64
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2016-11-11 13:24:55 +0100

    Logo på alle sidene og lik standard for tittel

---
+++ scripts/shopshowtest.js
@@ -0,0 +1,10 @@
+var pic=document.getElementById("picture")
+bildeArray=["shopmain.jpg","shop1.jpg","shop2.jpg","shop3.jpg"]
+i=1;
+setInterval(function(){
+	pic.src="img/shop/"+bildeArray[i];
+	i++;
+	if(i==4){
+		i=0;
+	}
+},5000)
