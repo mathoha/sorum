973b98b643cba228a332909c86c5f216f1bb4ee1
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2016-11-13 16:04:16 +0100

    opprydding

--- scripts/shopshow.js
+++ scripts/shopshow.js
@@ -1,3 +1,11 @@
+/*
+FILE NAME: shopshow.js
+WRITTEN BY: Reidar Johannessen
+EDITED BY: Åsmund Staldvik
+WHEN: November 2016
+PURPOSE: A edited version of slideshow.js to loop through an array with pictures of the store.
+*/
+
 var pic=document.getElementById("picture")
 bildeArray=["shopmain.jpg","shop1.jpg","shop2.jpg","shop3.jpg"]
 i=1;
